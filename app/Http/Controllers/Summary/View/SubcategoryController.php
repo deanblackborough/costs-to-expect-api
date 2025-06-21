44fd025f9db11d9561eefc7d80417d4b17d2850a
Dean Blackborough <dean@g3d-development.com>
2025-06-22 00:02:33 +0100

    Missed commit

--- app/Http/Controllers/Summary/View/SubcategoryController.php
+++ app/Http/Controllers/Summary/View/SubcategoryController.php
@@ -41,14 +41,14 @@ class SubcategoryController extends Controller
         $cache_summary->setFromCache($cache_control->getByKey($request->getRequestUri()));
 
         if ($cache_control->isRequestCacheable() === false || $cache_summary->valid() === false) {
-            $search_parameters = Parameter\Search::fetch(
-                Config::get('api.subcategory.summary-searchable')
-            );
+
+            $searchRequestService = new Parameter\Search($request->get('search'));
+            $searchParameters = $searchRequestService->fetch(Config::get('api.subcategory.summary-searchable'));
 
             $summary = (new Subcategory())->totalCount(
                 $resource_type_id,
                 $category_id,
-                $search_parameters
+                $searchParameters
             );
 
             $total = 0;
@@ -69,7 +69,7 @@ class SubcategoryController extends Controller
             $headers
                 ->addCacheControl($cache_control->visibility(), $cache_control->ttl())
                 ->addETag($collection)
-                ->addSearch(Parameter\Search::xHeader());
+                ->addSearch($searchRequestService->xHeader());
 
             if ($last_updated !== null) {
                 $headers->addLastUpdated($last_updated);
